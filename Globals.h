b8ad58fbc1e8bb4d994cb8807def6f84489c0cfc
Karel Jilek <los.karlosss@gmail.com>
2024-10-01 18:11:41 +0200

    some fixes

--- Globals.h
+++ Globals.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#define INFINITO 1000000000.0
 #define GREATER(A, B) (A > B)
 #define LESS(A, B) (A < B)
 #define EQUAL(A, B) (A == B)
@@ -8,4 +7,3 @@
 #define LESS_EQUAL(A, B) (LESS((A),(B)) || EQUAL((A),(B)))
 #define MIN(A, B) (LESS((A),(B)) ? (A) : (B))
 #define MAX(A, B) (LESS((A),(B)) ? (B) : (A))
-
